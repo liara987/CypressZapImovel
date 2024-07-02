021e06ae54d004ff96cf589f403de5a2d5350bb7
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-07-02 00:43:33 -0300

    feature: add report

--- cypress-zap-imoveis/cypress.config.js
+++ cypress-zap-imoveis/cypress.config.js
@@ -4,10 +4,17 @@ export default defineConfig({
   defaultCommandTimeout: 20000,
   pageLoadTimeout: 20000,
   chromeWebSecurity: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: false,
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {      
     },
+    video: true,
     baseUrl: "https://www.zapimoveis.com.br/",
     viewportWidth: 1440, // Muda viewport para deixar todos os itens superior visivel
     testIsolation: false, // Não limpa a tela a cada it
